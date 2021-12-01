f4ddd78d4da1cd3ddb10c65602bb56e81fe49b0c
Evgenia Kurineva <kurineva85@mail.ru>
2021-12-01 16:25:16 +0300

    feat: add types for loader.ts

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,21 +1,23 @@
-class Loader {
-    constructor(baseLink, options) {
+import { ENDPOINTS, IOptions, STATUS_CODE, ServerResponseData, METHOD } from './../../types/common';
+
+abstract class Loader {
+    constructor(private baseLink: string, private options: IOptions) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
+        { endpoint, options = {} }: { endpoint: ENDPOINTS; options: IOptions },
         callback = () => {
             console.error('No callback for GET response');
         }
-    ) {
-        this.load('GET', endpoint, callback, options);
+    ): void {
+        this.load(METHOD.GET, endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response): Response {
         if (!res.ok) {
-            if (res.status === 401 || res.status === 404)
+            if (res.status === STATUS_CODE.UNAUTHORIZED || res.status === STATUS_CODE.NOT_FOUND)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
             throw Error(res.statusText);
         }
@@ -23,10 +25,9 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    makeUrl(options: IOptions, endpoint: ENDPOINTS): string {
+        const urlOptions: { [index: string]: string } = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
-
         Object.keys(urlOptions).forEach((key) => {
             url += `${key}=${urlOptions[key]}&`;
         });
@@ -34,7 +35,12 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(
+        method: METHOD,
+        endpoint: ENDPOINTS,
+        callback: (data: ServerResponseData) => void,
+        options: IOptions = {}
+    ): void {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
