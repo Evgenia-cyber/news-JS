323d268ce0daa04205066efb0ae5eea1fd9a0fbe
Evgenia Kurineva <kurineva85@mail.ru>
2021-12-03 17:16:16 +0300

    feat: replace news api url to news-js-server url

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 abstract class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://news-js-server.herokuapp.com/', {
             apiKey: '19e9dbb0325c4420bbc0897b7c6629b2', // получите свой ключ https://newsapi.org/
         });
     }
